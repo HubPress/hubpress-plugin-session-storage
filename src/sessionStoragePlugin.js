da9064b87d18ec5323a6d255c2039e93601fb3ce
Anthonny <anthonny.querouil@gmail.com>
2016-03-06 01:54:02 +0100

    Initial import

---
+++ src/sessionStoragePlugin.js
@@ -0,0 +1,37 @@
+export function sessionStoragePlugin(hubpress) {
+
+  // store credentials in localstorage
+  hubpress.on('receiveAuthentication', opts => {
+    if (opts.data.authentication.isAuthenticated) {
+      sessionStorage.setItem('hp-credentials', JSON.stringify({
+        token: opts.data.authentication.token,
+        permissions: opts.data.authentication.permissions,
+        userInformations: opts.data.authentication.userInformations
+      }));
+    }
+
+    return opts;
+  });
+
+  hubpress.on('requestSavedAuth', opts => {
+    console.log('SessionStorage plugin requestSavedAuth', opts);
+    const authentication = {};
+    const storedData = sessionStorage.getItem('hp-credentials');
+    let credentials ={};
+    if (storedData) {
+      credentials = JSON.parse(storedData);
+      authentication.isAuthenticated = true;
+    }
+    authentication.credentials = credentials;
+
+    const mergeAuthentication = Object.assign({}, authentication, opts.data.authentication);
+    const data = Object.assign({}, opts.data, {authentication: mergeAuthentication});
+    return Object.assign({}, opts, {data});
+  })
+
+  hubpress.on('requestLogout', opts => {
+    sessionStorage.removeItem('hp-credentials');
+    return opts;
+  })
+
+}
