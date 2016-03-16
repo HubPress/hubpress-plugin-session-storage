b707e987006ca91c0991a31e02646f7f97497f63
Anthonny <anthonny.querouil@gmail.com>
2016-03-16 07:55:39 +0100

    fix(storage): change the format of the content in the storage

--- src/sessionStoragePlugin.js
+++ src/sessionStoragePlugin.js
@@ -3,9 +3,11 @@ export function sessionStoragePlugin(hubpress) {
   // store credentials in localstorage
   hubpress.on('receiveAuthentication', opts => {
     if (opts.data.authentication.isAuthenticated) {
-      sessionStorage.setItem(`${opts.state.application.config.meta.repositoryName}-credentials`, JSON.stringify({
-        token: opts.data.authentication.token,
-        permissions: opts.data.authentication.permissions,
+      sessionStorage.setItem(`${opts.state.application.config.meta.repositoryName}-authentication`, JSON.stringify({
+        credentials: {
+          token: opts.data.authentication.token,
+          permissions: opts.data.authentication.permissions
+        },
         userInformations: opts.data.authentication.userInformations
       }));
     }
@@ -15,14 +17,20 @@ export function sessionStoragePlugin(hubpress) {
 
   hubpress.on('requestSavedAuth', opts => {
     console.log('SessionStorage plugin requestSavedAuth', opts);
-    const authentication = {};
-    const storedData = sessionStorage.getItem(`${opts.data.config.meta.repositoryName}-credentials`);
-    let credentials ={};
+    let authentication;
+    const storedData = sessionStorage.getItem(`${opts.data.config.meta.repositoryName}-authentication`);
+
     if (storedData) {
-      credentials = JSON.parse(storedData);
+      authentication = JSON.parse(storedData);
       authentication.isAuthenticated = true;
     }
-    authentication.credentials = credentials;
+    else {
+     authentication = {
+       credentials: {},
+       userInformations: {},
+       isAuthenticated: false
+     };
+    }
 
     const mergeAuthentication = Object.assign({}, authentication, opts.data.authentication);
     const data = Object.assign({}, opts.data, {authentication: mergeAuthentication});
@@ -30,7 +38,7 @@ export function sessionStoragePlugin(hubpress) {
   })
 
   hubpress.on('requestLogout', opts => {
-    sessionStorage.removeItem(`${opts.state.application.config.meta.repositoryName}-credentials`);
+    sessionStorage.removeItem(`${opts.state.application.config.meta.repositoryName}-authentication`);
     return opts;
   })
 
